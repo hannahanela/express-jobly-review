44838e34c6e895d69042d924150ffebbc77c1429
anela <anela89@gmail.com>
2023-01-18 17:45:31 -0800

    Added documentation for sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,19 @@
+"use strict";
+
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Helper function for selectively updating data.
+ * 
+ * Takes user inputs and corresponding SQL columns, and generates an object of
+ *   DML and updated values.
+ * 
+ * @param dataToUpdate { Object } { numEmployees: 2, logoUrl: "url" }
+ * @param jsToSql { Object } maps JS key names to SQL column names like
+ *      { numEmployees: "num_employees", logoUrl: "logo_url" }
+ * @returns {Object} { setCols, values } where setCols is joined DML and values
+ *      is an array of user inputs like [2, "url"]
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);

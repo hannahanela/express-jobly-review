3d18cffcde9ab98b00a92b3d4c4f84986a774413
anela <anela89@gmail.com>
2023-01-19 16:11:07 -0800

    Wrote unit tests for sqlForPartialUpdate function
    All tests passing.

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,2 +1,30 @@
 "use strict";
 
+const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
+
+describe("sqlForPartialUpdate", function () {
+  it("works for 1 item", function () {
+    const result = sqlForPartialUpdate(
+      { testData: "Test" },
+      { testData: "test_data" }
+    );
+
+    expect(result).toEqual({
+      setCols: `"test_data"=$1`,
+      values: ["Test"],
+    });
+  });
+
+  it("works for more than 1 item", function () {
+    const result = sqlForPartialUpdate(
+      { testName: "Test", testNum: 1 },
+      { testName: "test_name", testNum: "test_num" }
+    );
+
+    expect(result).toEqual({
+      setCols: `"test_name"=$1, "test_num"=$2`,
+      values: ["Test", 1],
+    });
+  });
+});
